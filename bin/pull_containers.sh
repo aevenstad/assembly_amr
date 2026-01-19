59fd4870ed10fab0263e13b01ba47664d15770ef
Andreas E <77004553+aevenstad@users.noreply.github.com>
2026-01-19 13:10:18 +0100

    Remove unused container images from pull script
    Removed several container images from the list.

--- bin/pull_containers.sh
+++ bin/pull_containers.sh
@@ -4,15 +4,7 @@
 containers=(
     "https://depot.galaxyproject.org/singularity/ncbi-amrfinderplus:4.0.19--hf69ffd2_0"
     "https://depot.galaxyproject.org/singularity/bakta:1.10.4--pyhdfd78af_0"
-    "https://depot.galaxyproject.org/singularity/fastp:0.23.4--h5f740d0_0"
-    "https://depot.galaxyproject.org/singularity/fastqc:0.12.1--hdfd78af_0"
-    "docker://quay.io/gbouras13/hybracter:0.11.0"
-    "https://depot.galaxyproject.org/singularity/kleborate:3.1.2--pyhdfd78af_0"
-    "https://depot.galaxyproject.org/singularity/mlst:2.23.0--hdfd78af_0"
-    "https://depot.galaxyproject.org/singularity/nanostat:1.6.0--pyhdfd78af_0"
     "https://depot.galaxyproject.org/singularity/plasmidfinder:2.1.6--py310hdfd78af_1"
-    "https://depot.galaxyproject.org/singularity/quast:5.2.0--py39pl5321heaaa4ec_4"
-    "https://depot.galaxyproject.org/singularity/shovill:1.1.0--0"
 )
 
 # Set container directory argument
